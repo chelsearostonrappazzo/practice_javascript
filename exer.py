b909bdf0af53aa08ddda43a4d52efcd5464375a9
Chelsea Roston <cmroston@gmail.com>
2021-03-19 23:30:45 -0400

    adds python translations for second exercise

--- exer.py
+++ exer.py
@@ -65,3 +65,90 @@
 
 
 # join_strings(list)
+
+# Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+
+# def product(numList):
+#     product = 1
+#     for num in numList:
+#         product *= num
+#     print(product)
+
+
+# numList = [2, 3, 1, 6, 5, ]
+
+# product(numList)
+
+# Write a function that takes in an array of numbers and returns the two smallest numbers.
+
+# numList = [2, 3, 1, 4]
+
+
+# def twoSmalls(numList):
+#     firstMin = secondMin = 999
+#     for num in numList:
+#         if num < firstMin:
+#             secondMin = firstMin
+#             firstMin = num
+#         elif num < secondMin:
+#             secondMin = num
+#     print(firstMin, secondMin)
+
+
+# twoSmalls(numList)
+
+# Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+
+# numList = [0, 3, 4, 0, 2, 5, 0]
+
+
+# def zeroCounter(numlist):
+#     counter = 0
+#     for num in numList:
+#         if num == 0:
+#             counter += 1
+#     print(counter)
+
+
+# zeroCounter(numList)
+
+# Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+
+# numList1 = [5, 6, 3, 10, 12]
+# # output false
+# numList2 = [11, 24, 20, 54]
+# # output true
+
+
+# def greaterThanTen(numList):
+#     i = 0
+#     list = []
+#     while i < len(numList):
+#         if numList[i] > 10:
+#             list.append(numList[i])
+#         i += 1
+#     if list == numList:
+#         print("True")
+#     else:
+#         print("False")
+
+
+# greaterThanTen(numList1)
+# greaterThanTen(numList2)
+
+
+list = ["apple", "aardvark", "anime"]
+list2 = ["bomb", "money", "desert"]
+
+
+def aCounter(list):
+    counter = 0
+    for word in list:
+        for letter in ([char for char in word]):
+            if letter == "a":
+                counter += 1
+    print(counter)
+
+
+aCounter(list)
+aCounter(list2)
